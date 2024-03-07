bb2772d027cc39476e895c53dfa71a34204111ac
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-07 15:50:18 +0100

    import assets @vite() on head page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/js/app.js')
 </head>
-<body>
-<h1>test</h1>
-    
+<body>  
 </body>
-</html>
+</html><
