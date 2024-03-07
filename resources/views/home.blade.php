9ab3885a850bcd58dbf232d43e9ed5bf3aac2881
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-07 19:22:36 +0100

    add  comics image with config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,6 +29,13 @@
     <!--Section Main-->
     <section>
         <div class=" card-container container">
+           @foreach (config('comics') as $comic) 
+            <div class="card">
+                <img src="{{$comic['thumb']}}" alt="">
+                <h5>{{ $comic['series'] }}</h5>
+                
+            </div>  
+            @endforeach        
         </div>
     </section>
     <!--LINK FOOTER-->
