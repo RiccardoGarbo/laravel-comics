33b6a3632337cb722914bad41831557053f873a2
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-08 17:05:47 +0100

    add tag a on card and add route

--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -1,10 +1,12 @@
 <section>
     <div class=" card-container container">
-       @foreach (config('comics') as $comic) 
+       @foreach (config('comics') as $index => $comic) 
+       <a href="{{route('comic.show', ['index' => $index])}}">
         <div class="card">
             <img src="{{$comic['thumb']}}" alt="">
             <h5>{{ $comic['series'] }}</h5>  
         </div>  
-        @endforeach        
+    </a>
+        @endforeach                                                        
     </div>
 </section>
