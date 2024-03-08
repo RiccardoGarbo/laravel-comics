4aa19e9585f82785b8f13b552f5847d33f9e459f
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-08 17:55:38 +0100

    add elements and style comic page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,2 +1,24 @@
 @extends('layouts.app')
 
+@section('main-content')
+<div id="show">
+    <div id="description">
+        <h1>{{$comic['title']}}</h1>
+        <div id="availability">
+            <div>
+        <span>U.S Price {{$comic['price']}}<span>
+        </div>
+        <div>
+            <span>AVAILABLE</span>
+           <button>Check Availability</button>
+        </div>
+
+    </div>
+        <p>{{$comic['description']}}</p>
+    </div>
+        <div id="image">
+            <img src="{{$comic['thumb']}}" alt="">
+        </div>
+</div>
+
+@endsection
